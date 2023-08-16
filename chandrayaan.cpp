bb36983aa46b46c7e4138ec851861001f87e5c97
ShourjoSengupta <shourjosenguptaroop@gmail.com>
2023-08-16 13:28:49 +0530

    commit for backward and left

--- chandrayaan.cpp
+++ chandrayaan.cpp
@@ -2,19 +2,52 @@
 #include <vector>
 using namespace std;
 
-class Chandrayaan {
+class Chandrayaan
+{
 public:
     Chandrayaan(int initialX, int initialY, int initialZ, char initialDirection)
         : x(initialX), y(initialY), z(initialZ), direction(initialDirection) {}
 
-void forward() {
-        if (direction == 'N') y++;
-        else if (direction == 'S') y--;
-        else if (direction == 'E') x++;
-        else if (direction == 'W') x--;
-        else if (direction == 'U') z++;
-        else if (direction == 'D') z--;
+    void forward()
+    {
+        if (direction == 'N')
+            y++;
+        else if (direction == 'S')
+            y--;
+        else if (direction == 'E')
+            x++;
+        else if (direction == 'W')
+            x--;
+        else if (direction == 'U')
+            z++;
+        else if (direction == 'D')
+            z--;
     }
+
+    void backward()
+    {
+        if (direction == 'N')
+            y--;
+        else if (direction == 'S')
+            y++;
+        else if (direction == 'E')
+            x--;
+        else if (direction == 'W')
+            x++;
+        else if (direction == 'U')
+            z--;
+        else if (direction == 'D')
+            z++;
+    }
+    void left() {
+        if (direction == 'N') direction = 'W';
+        else if (direction == 'S') direction = 'E';
+        else if (direction == 'E') direction = 'N';
+        else if (direction == 'W') direction = 'S';
+        else if (direction == 'U') direction = 'N';
+        else if (direction == 'D') direction = 'S';
+    }
+
 
 public:
     int x, y, z;
--- test.cpp
+++ test.cpp
@@ -13,10 +13,29 @@ int main() {
     cout << "Chandrayaan constructor test passed!" << endl;
 
     obj.forward();
-    assert(obj.x == 1);
+    assert(obj.x == 0);
+    assert(obj.y == 1);
+    assert(obj.z == 0);
+    
+
+    cout << "Chandrayaan forward movement test passed!" << endl;
+
+    obj.backward();
+    assert(obj.x == 0);
     assert(obj.y == 0);
     assert(obj.z == 0);
 
-    cout << "Chandrayaan forward movement test passed!" << endl;
+
+    cout << "Chandrayaan backward movement test passed!" << endl;
+
+    obj.left();
+    assert(obj.direction == 'W');
+    assert(obj.x == 0);
+    assert(obj.y == 0);
+    assert(obj.z == 0);
+
+
+    cout << "Chandrayaan left movement test passed!" << endl;
+
     return 0;
 }
